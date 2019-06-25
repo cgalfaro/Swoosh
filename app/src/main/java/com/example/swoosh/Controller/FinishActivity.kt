1305aa2adb2e37932331ef43ffaf50bfc37fa643
Carlos Alfaro <carlosalfaro@finblok.tech>
2019-06-25 12:21:59 -0600

    created parcelable class "Player" and passed this from activity to activity deleted constants that were the strings that we passed before.

--- app/src/main/java/com/example/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/FinishActivity.kt
@@ -3,9 +3,9 @@ package com.example.swoosh.Controller
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.swoosh.Model.Player
 import com.example.swoosh.R
-import com.example.swoosh.Utilities.EXTRA_LEAGUE
-import com.example.swoosh.Utilities.EXTRA_SKILL
+import com.example.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
@@ -15,9 +15,8 @@ class FinishActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
-        val league = intent.getStringExtra(EXTRA_LEAGUE)
-        val skill = intent.getStringExtra(EXTRA_SKILL)
+        val player = intent.getParcelableExtra<Player>(EXTRA_PLAYER)
 
-        searchLeaguesTxt.text = "Looking for $league $skill league near you..."
+        searchLeaguesTxt.text = "Looking for ${player.league} ${player.skill} league near you..."
     }
 }
